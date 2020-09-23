05b3c9eab0b0555306d26ae0a8281699d6384ebc
Ralf <ralfvb97@gmail.com>
2020-09-23 17:15:37 +0200

    PlayerData-Script created

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public int m_CurrentHealth;
+    public int m_MaxHealth;
+    public float[] m_Position;
+
+    public PlayerData(PlayerController _controller)
+    {
+        m_CurrentHealth = _controller.m_currentHealth;
+        m_MaxHealth = _controller.m_maxHealth;
+
+        m_Position = new float[3];
+        m_Position[0] = _controller.transform.position.x;
+        m_Position[1] = _controller.transform.position.y;
+        m_Position[2] = _controller.transform.position.z;
+    }
+}
